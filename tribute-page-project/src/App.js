1fc2cc88973076c09f3909b1863330fe85f712de
Blessedbrita <blessedmasunika@gmail.com>
2024-04-09 23:00:25 +0200

    file upload

---
+++ tribute-page-project/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TributePage from './TributePage';
+
+function App() {
+  return (
+    <div className="App">
+      <TributePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ tribute-page-project/src/TributePage.js
@@ -0,0 +1,162 @@
+import React from "react";
+
+
+export default function TributePage() {
+
+    return (
+        <div>
+            <div className="logo">
+                <div className="row">
+                    <div className="col-2">
+                        <img src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/116/871/original/Screenshot_27-2-2024_162816_turbologo.com.jpeg?1709044624" alt="Martin" width="90" className="img-fluid" />
+                    </div>
+                    <div className="col-10">
+                        <p className="intro">
+                            M A R T I N &#x2022; L U T H E R &#x2022; K I N G &#x2022; J R
+                            <br />
+                            <span className="date">15 January 1929 - 4 April 1968</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-6">
+                    <img src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/119/754/original/Martin.jpg?1710835014" alt="Martin" width="450" className="cover" />
+                </div>
+                <div className="col-5 quote">
+                    <p className="quote1">
+                        "If you can't fly, then run.
+                        <br />
+                        If you can't run, then walk.
+                        <br />
+                        If you can't walk, then crawl,
+                        <br />
+                        but whatever you do, YOU HAVE TO KEEP MOVING."
+                        <br />
+                        <span className="name">- Martin Luther King Jr.</span>
+                    </p>
+                </div>
+            </div>
+
+            <div className="timeline">
+                <br />
+                <h3 className="bio">Biography</h3>
+                <div className="row">
+                    <div className="col-10">
+                        <p className="biography">
+                            <strong> Martin Luther King Jr. was born in Atlanta, Georgia, in 1929</strong>, and emerged as a prominent figure in the American civil rights movement. His dedication to nonviolent resistance and his eloquent speaking abilities thrust him into the forefront of the battle against racial segregation and bias. Through initiatives such as the Montgomery Bus Boycott and the establishment of the Southern Christian Leadership Conference, King orchestrated peaceful protests and rallies throughout the Southern states, inspiring countless individuals with his call for equality and fairness. His memorable <strong>"I Have a Dream" </strong>speech, delivered during the 1963 March on Washington, remains one of the most renowned addresses in American history. King's unwavering advocacy for civil rights led to his receipt of the Nobel Peace Prize in 1964, yet his life was tragically cut short by an assassination in 1968. His enduring legacy as an emblem of optimism, bravery, and the pursuit of justice continues to reverberate globally.
+                        </p>
+                    </div>
+                </div>
+                <br />
+                <h1 className="heading">T i m e l i n e</h1>
+
+                <div className="container" id="timeline">
+                    <ul>
+
+                        <li>
+                            <i />
+                            <div className="box">
+                                <h2 className="title"><span className="year">1929</span> Birth</h2>
+                                <p>Martin Luther King Jr. is born on January 15th in a family home on 501 Auburn Avenue Atlanta, Georgia.</p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <i />
+                            <div className="box">
+                                <h2 className="title"><span className="year">1944</span> Begins freshman year</h2>
+                                <p>Martin graduates from Booker T. Washington High School at the age of 15 and enrolls in Morehouse College. He originally studies medicine and law but later decides to enter the ministry. </p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <i />
+                            <div className="box">
+                                <h2 className="title"><span className="year">1948</span> Graduates from college</h2>
+                                <p>He receives a Bachelor of Arts degree in sociology from Morehouse College. He spends the next three years at Crozer Theological Seminary in Chester, Pennsylvania. It is here that he first learns about Mahatma Gandhi’s philosophy of non-violence.</p>
+                            </div>
+                            <img src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/119/767/original/636506861281833620-XXX-MLK-MOREHOUSE-COLLEGE-GRADUATION-96319291.jpg?1710848005" alt="Martin" width="150" className="grad1 d-none d-lg-block" />
+                        </li>
+
+                        <li>
+                            <i />
+                            <div className="box">
+                                <h2 className="title"><span className="year">1951</span> Graduates as a valedictorian</h2>
+                                <p>He completes his Bachelor of Divinity degree at Crozer Theological Seminary in Pennsylvania. King is elected president of Crozer’s student body, which is composed almost entirely of white students. He is the valedictorian of his class in 1951. King begins his graduate studies in systematic theology at Boston University.</p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <i />
+                            <div className="box">
+                                <h2 className="title"><span className="year">1953</span> Marriage</h2>
+                                <p>King and Coretta Scott are married at the Scott home near Marion, Alabama.</p>
+                            </div>
+                            <img src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/119/773/original/martin_wed.jpeg?1710848997" alt="Martin" width="250" className="grad1 d-none d-lg-block" />
+                        </li>
+
+                        <li>
+                            <i />
+                            <div className="box">
+                                <h2 className="title"><span className="year">1955</span> The Montgomery Bus Boycott</h2>
+                                <p>King led the boycott, which lasted 385 days and was in response to Rosa Park’s arrest under the segregation laws. Following a 1956 ruling, it ended when the Supreme Court announced that segregation on public buses was unconstitutional.</p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <i />
+                            <div className="box">
+                                <h2 className="title"><span className="year">1957</span> The founding of the Southern Christian Leadership Conference (SCLC)</h2>
+                                <p>To better coordinate activities across the south, King founded the organization. The combined efforts of sixty black ministers and civil rights leaders provided more unified ways of campaigning. The harmonious call for equality rang more clearly and to more ears than ever before. </p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <i />
+                            <div className="box">
+                                <h2 className="title"><span className="year">1963</span> The Great March on Washington</h2>
+                                <p>King was one of the leaders that arranged the March on Washington for job creation and freedom. The march took place in Washington, D.C.with over 250,000 participants. This is when he delivers his iconic "I Have a Dream" speech during the March on August 28th.</p>
+                            </div>
+                            <img src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/119/774/original/dream.jpeg?1710849999" alt="Martin" width="200" className="grad d-none d-lg-block" />
+                        </li>
+
+                        <li>
+                            <i />
+                            <div className="box">
+                                <h2 className="title"><span className="year">1964</span> Civil Rights Act of 1964</h2>
+                                <p>This act ended segregation in public places and banned discrimination based on race, color, religion, sex, or national origin. It is considered one of the primary legislative achievements of the civil rights movement. King’s activism helped to shape the qualifiers of the act. In the same year, King receives a Nobel Peace Prize “for his non-violent struggle for civil rights.” King stated, “that this Nobel Prize was won by a movement of great people, whose discipline, wise restraint, and majestic courage.”</p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <i />
+                            <div className="box">
+                                <h2 className="title"><span className="year">1965</span> Voting Rights Act</h2>
+                                <p>King led marches in Selma, Alabama, dramatizing the voting inequality issue. It prompted President Johnson to send a bill to Congress to remove race-based restrictions. After passing into law in 1965, the act outlawed discriminatory voting practices adopted in many southern states. It prohibited unfair practices such as literacy tests as a prerequisite to voting. </p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <i />
+                            <div className="box">
+                                <h2 className="title"><span className="year">1968</span> Assassination</h2>
+                                <p>King travels to Memphis, Tennessee, to support striking sanitation workers on April 4th and is assassinated by James Earls Ray, who later pleads guilty and is sentenced to prison for 99 years. King dies at Joseph's hospital an hour later after the shooting. </p>
+                            </div>
+                            <img src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/119/776/original/dead.jpeg?1710850959" alt="Martin" width="200" className="grad1 d-none d-lg-block" />
+                        </li>
+
+                    </ul>
+                </div>
+
+                <br />
+                <div className="testimonial">
+                    <p>Martin Luther King Jr.'s legacy as the greatest and most loved leader stems from his moral clarity, visionary leadership, and unwavering dedication to the principles of justice, equality, and nonviolence. His impact on society continues to be felt, inspiring generations to work towards a more inclusive and equitable world.</p>
+                </div>
+            </div>
+
+
+        </div>
+
+    )
+}
